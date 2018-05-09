a356eb74e5c4bc9c51e22c22089b295500f4cecc
prshant1 <39118269+prshant1@users.noreply.github.com>
2018-05-09 15:52:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+require('./instantHello');
+var goodbye = require('./Talk/goodBye');
+var talk = require('./Talk');
+var question = require('./talk/question');
+
+var ans = question.ask("what is the meaning of Life?");
+console.log(ans);
+
+talk.hello('Verma');
+talk.intro();
+goodbye();
